36cd8e2d7c998d2b8859704c8aa395164efa0423
Iván Martín <ivanmartinga@gmail.com>
2017-05-05 20:45:25 +0200

    Data generation

--- amper/management/commands/generate_reports.py
+++ amper/management/commands/generate_reports.py
@@ -14,9 +14,10 @@ class Command(BaseCommand):
         base = datetime.today()
         num_days = 90
 
-        date_list = [base - timedelta(days=x) for x in range(0, num_days)]
+        date_list_past = [base - timedelta(days=x) for x in range(0, num_days)]
+        date_list_future = [base + timedelta(days=x) for x in range(0, num_days)]
 
-        for date in date_list:
+        for date in date_list_past:
             for x in range(3):
                 date = datetime(year=date.year, month=date.month, day=date.day, hour=randrange(10, 23),
                                 second=date.second, minute=date.minute)
@@ -25,6 +26,19 @@ class Command(BaseCommand):
                 Report.objects.create(
                     item=item,
                     duration=randrange(0, 60),
-                    consumption=item.consumption,
+                    consumption=250 - randrange(0, 500),
+                    start_time=date,
+                )
+
+        for date in date_list_future:
+            for x in range(3):
+                date = datetime(year=date.year, month=date.month, day=date.day, hour=randrange(10, 23),
+                                second=date.second, minute=date.minute)
+                item = query[randrange(1, query.count())]
+
+                Report.objects.create(
+                    item=item,
+                    duration=randrange(0, 60),
+                    consumption=250 - randrange(0, 500),
                     start_time=date,
                 )
